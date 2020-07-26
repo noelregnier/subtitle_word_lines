acc53a52f5661474a8ae1b1b7af85641db6447fc
noelregnier <noel.regnier@gmail.com>
2020-07-26 23:32:11 +0300

    version 2

--- word_examples.py
+++ word_examples.py
@@ -1,99 +1,85 @@
 import re
 
-#how many times each word happens in the the TV series
+all_lines = []
 
-all_lines = []   #list with text lines from subtitles
-words = []    #list with separate words from all_lines
-counts = {}   #dictionary to count word's frequencies
-
-
-try:
-    with open("combined_file.txt", "r") as file:
-        line = file.readline()
-        while line !='':
-            if not (re.findall('[0-9]', line) or re.findall('^\n', line)):
-                # line = line.strip().lstrip("<i>").rstrip("</i>")
+# get rid of lines which start with a digit or a whitespace and add the rest to all_lines
+def refine_lines():
+    try:
+        with open("combined_file.txt", "r") as file:
+            for line in file:
+                if (re.search('^[0-9]+', line) or re.search('^\n', line)):
+                    continue
                 all_lines.append(line)
-            line = file.readline()
-except:
-    print("combined_file.txt is not found. Please run create_combined_file.py first")
-    quit()
 
-# make separated words
-for item in all_lines:
-    separated_items = item.split()
-    for m in separated_items:
-        words.append(m)
+    except:
+        print("combined_file.txt is not found. Please run create_combined_file.py first")
+        quit()
 
-for word in words:
-    word = word.lower().strip().lstrip("<i>").rstrip("</i>").strip('"').rstrip(",").rstrip(".").rstrip("!").rstrip("?")
-    counts[word] = counts.get(word, 0) + 1
-
-
-# #Result1 - print all words with frequencies
-# for b in sorted([(v,k) for k,v in counts.items()], reverse=True):
-#     print(b)
-
-# Result2 - how many times a word happens in the subtitles and exact lines with it
-# check if the word entered by a user is among lines
-while True:
-    guess_input = input("Please enter a word or 'quit' to exit ")
-    guess = guess_input.lower()
-    if guess == "quit": quit()
-    if guess not in counts:
-        print("The word is not found.")
-        continue
-
-    # print("For '{}' we have these results: {}".format(guess_input, counts[guess]))
-
-
-    #print all lines with the desired word guess
-    index = []
+#store index fore lines in which the searched word occurs.
+def check_word(word):
+    guess = word
     for i, line in enumerate(all_lines):
         line = line.lower()
-        strict = re.findall('^'+guess+'[^a-z]', line)
-        if len(strict) == 0:
-            strict = re.findall('[^a-z]' + guess + '[^a-z]', line)
-        if len(strict) > 0:
+        #word should not be a part of another word in a line
+        if (re.search('^' + guess + '[^a-z]+', line) or re.search('[^a-z]+' + guess + '[^a-z]+', line)):
             index.append(i)
 
+def print_picked_lines():
+    if len(index) == 0:
+        print("No results found")
+    else:
+        print("The results are:" + "\n")
+        number = 0
 
-    #print found lines
-    print("The results are:"+"\n")
-    number = 0
+        for i in index:
+            number = number + 1
+            sentence = []
+            big = i
+            while True:
+                if big == (len(all_lines)-1): break
+                line = all_lines[big]
+                sentence.append(line)
+                line = line.strip()
+                if re.search("[.!?>]$", line): break
 
-    for i in index:
-        number = number+1
-        sentence = []
-        k = i
-        ss = True
-        while ss:
-            line = all_lines[k]
-            sentence.append(line)
-            line = line.strip()
-            k = k+1
-            if (line.endswith(".") or line.endswith("!") or line.endswith("?") or line.endswith("?!")):
-                ss = False
-        k = i
-        while True:
-            k = k - 1
-            line = all_lines[k]
-            line1 = line.strip()
-            if (line1.endswith(".") or line1.endswith("!") or line1.endswith("?") or line1.endswith("?!")):
-                break
-            sentence.insert(0, line)
+                # if (line.endswith(".") or line.endswith("!") or line.endswith("?") or line.endswith(".</i>") or line.endswith("?</i>") or line.endswith("!</i>") or line.endswith(")</i>")):
+                #     break
+                big = big + 1
 
-        sen = ""
+            small = i - 1
+            while True:
+                if small < 0 : break
+                line_raw = all_lines[small]
+                line = line_raw.rstrip()
+                if re.search("[.!?>]$", line): break
 
-        for m in sentence:
-            sen = sen + m
-        print("Result {}:".format(number))
-        print(sen)
-        print("----------------------------")
-
-
+                # if (line.endswith(".") or line.endswith("!") or line.endswith("?") or line.endswith(".</i>") or line.endswith("?</i>") or line.endswith("!</i>") or line.endswith(")</i>")):
+                #     break
+                sentence.insert(0, line)
+                small = small - 1
 
 
-        
+            sen = ""
+
+            for m in sentence:
+                sen = sen + m
+            print("Result {}:".format(number))
+            print(sen)
+            print("----------------------------")
+
+#execution
+refine_lines()
+while True:
+    word = input("Please enter a word or 'quit' to exit ")
+    word = word.lower()
+    if word == "quit": quit()
+    index = []
+    check_word(word)
+    print_picked_lines()
+
+
+
+
+
 
 
